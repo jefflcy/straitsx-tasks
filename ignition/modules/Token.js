5a8d2c4065f80f09f939023337e3f276295e12ac
jepp <108892550+jefflcy@users.noreply.github.com>
2025-05-23 12:26:44 +0800

    Add hardhat ignition deployment script

---
+++ ignition/modules/Token.js
@@ -0,0 +1,11 @@
+
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+const TokenModule = buildModule("TokenModule", (m) => {
+  const token = m.contract("Token");
+
+  return { token };
+});
+
+module.exports = TokenModule;
+
